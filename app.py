ae4660fe743443ec3b523a53d8fbacbb5c406473
allankiplangat <allankiplangat22@gmail.com>
2018-02-25 23:09:38 +0300

    Advanced parsing with flask

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'allan'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) #/auth
+
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -20,6 +26,28 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required = True,
+            help = "This field cannot be left blank"
+        )
+        data = parser.parse_args()
+        
+        item = list(filter(lambda x: x['name'] != name, items))
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
