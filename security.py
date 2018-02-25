474582992e9dcfdd20c72690365c18b927dbc2c9
allankiplangat <allankiplangat22@gmail.com>
2018-02-25 19:23:08 +0300

    Authentication and logging part one

--- security.py
+++ security.py
@@ -1,31 +1,17 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
-    {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
+    User(1, 'bob', 'asdf')
 ]
 
-username_mapping = {'bob': {
+username_mapping = {u.username: u for u in users} #Set comprehension
+userid_mapping = {u.id: u for u in users}
 
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
-
-userid_mapping = { 1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
